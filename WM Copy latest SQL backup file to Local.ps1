d0be047e3e158291dd1c9ad9e6deff1789aec1f2
Guy Harwood <accounts@harwood.co.za>
2021-07-16 15:38:32 +0200

    ?

--- WM Copy latest SQL backup file to Local.ps1
+++ WM Copy latest SQL backup file to Local.ps1
@@ -1,4 +1,4 @@
-﻿#Warren Marine - Guy Harwood
+#Warren Marine - Guy Harwood
 #Purpose: To copy the SQL database file from Server location to local USB drive & Dropbox.
 #
 #
@@ -19,26 +19,3 @@ if ($vpnok.ConnectionStatus -eq 'connected')
 
             copy-item -destination $destpath -Force
             copy-item -path "$destpath\circe*" -destination $dropbox -Force
-     }
-    elseif($locallconn.PingSucceeded -eq 'True'){
-            $sourcepath = "\\wserver\backup"
-            $destpath = "D:\Circe Launches Backups\SQL Circe Bookings"
-            $dropbox ="C:\Users\Guy\Dropbox\SCS\Circe Launches"
-
-            Remove-Item -path "$destpath\circe*"
-            Remove-item -Path "$dropbox\circe*"
-
-        Get-ChildItem -path "$sourcepath\circe*" -file | 
-            Sort-Object -Property Modifiedtime -Descending | Select-Object -First 1 | 
-
-            copy-item -destination $destpath -Force
-            copy-item -path "$destpath\circe*" -destination $dropbox -Force
-
-  }   
-    
-    else 
-    {
-        
-        Guy-SendGmail "Copy of SQL backup file to USB drive & Dropbox failed" "PLEASE INVESTIGATE"
-        
-   }
